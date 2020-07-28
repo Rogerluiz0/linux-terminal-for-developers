aadc5be719698e350a58b12a87fd2c35590a4c7d
Roger Luiz <Rogerluiz882@gmail.com>
2020-07-28 10:43:44 -0300

    Back-end updated

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
-const express = require( 'express' )
-const path = require( 'path' )
 const router = require('express').Router()
-const controller = require('@controller/index.js')
-
-router.use( express.static( path.join( __dirname, '/../public' ) ) )
+const controller = require('./controller/index.js')
 
 router.get( '/', controller.renderHome )
 router.get( '/terminal', controller.renderTerminal )
