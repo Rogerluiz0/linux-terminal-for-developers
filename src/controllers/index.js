7c8ea18b773af2a6aaca5bc0d249a664eeae30db
Roger Luiz <Rogerluiz882@gmail.com>
2020-09-16 16:01:52 -0300

    Update 4.0.0

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,17 +1,9 @@
-const path = require( 'path' )
-
 const controller = {
-  pageHome( req, res ) {
+  pageHome( req , res ) {
     res.render( 'index.html' )
   },
-  pageTerminal( req, res ) {
-    res.render( 'terminal.html' )
-  },
-  pageAbout( req, res ) {
-    res.render( 'about.html' )
-  },
-  page404( req, res ) {
-    res.render( '404-error.html' )
+  page404( req , res ) {
+    res.render( 'pageNotFound.html' ) 
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -6,8 +6,6 @@ const config = require( './config/index.js' )
 const PORT = config.app.port
 
 app.listen( PORT , ( err ) => {
-  if ( err ) {
-    console.log( 'erro' )
-  }
+  if ( err ) { console.log( 'erro' ) }
   console.log( `Server running on localhost:${PORT}` )
 } )
