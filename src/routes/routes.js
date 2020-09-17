a80ed903a81fd969a9e001ab9f91e7db7cafe36f
Roger Luiz <Rogerluiz882@gmail.com>
2020-09-17 16:25:36 -0300

    descontinuado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,5 @@ const controller = require('./../controllers/index.js')
 const { pageHome , page404 } = controller
 
 router.get( '/' , pageHome )
-router.get( '*' , page404 ) // Esta rota deve sempre ser a última!
 
 module.exports = router
